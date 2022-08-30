ea5a626e998f7546c2264fe8be3f615e2bcd58f8
lucas170198 <lucassilvaamorim1998@gmail.com>
2022-08-29 22:13:25 -0300

    Implementation v1

---
+++ csv-reader/src/Lib.hs
@@ -0,0 +1,120 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -Wno-unticked-promoted-constructors #-}
+
+module Lib
+    ( 
+    -- somefunc
+    ) where
+
+import qualified Data.ByteString.Lazy as BL
+import Data.ByteString.Internal (w2c)
+import Data.Kind
+
+
+-- | Some Helpers functions
+split :: Char -> String -> [String]
+split _ "" = [""]
+split delim (x : xs) | x == delim = "" : (split delim xs)
+                     | otherwise  = (x : head (split delim xs)) : tail (split delim xs)
+
+-- | Using GADT's to choose the encode mode (name indexed or number indexed).
+
+-- | Here, mode is a panthom type, just to choose the compression mode.
+type Name = String
+type Index = Int
+
+
+data CSVFile mode cols lin where
+    NameIndexed :: Vector cols String -> Vector lin (Vector cols String) -> CSVFile Name cols lin
+    NumberIndexed :: Vector lin (Vector cols String) -> CSVFile Index cols lin
+deriving instance Show (CSVFile m c l)
+
+-- | User indicates which type of compression mode we want
+data FileType opt where
+    Header :: FileType String
+    NoHeader :: FileType Int
+deriving instance Show (FileType opt)
+deriving instance Eq (FileType opt)
+
+-- | Abstraction of ByteString, to deal with Strings on the logic
+-- class Container a where
+--     type Elem a
+--     elements :: a -> [Elem a]
+
+-- instance Container BL.ByteString where
+--     type Elem BL.ByteString = Char
+--     elements a = fmap w2c (BL.unpack a)
+
+
+-- | Safe access to Rows and columns
+data Nat = Z | S Nat
+    deriving Eq
+
+type Vector :: Nat -> Type -> Type
+data Vector n a where
+    VNil :: Vector 'Z a
+    (:>) :: a -> Vector n a -> Vector ('S n) a
+infixr 5 :>
+deriving instance Show a => Show (Vector n a)
+
+-- | Finity GADT's. Used to generate possible valid index
+data Fin :: Nat -> Type where
+    FinZ :: Fin ('S n) -- 0th index to Vec n non-enpty
+    FinS :: Fin n -> Fin ('S n)
+deriving instance Show (Fin n)
+
+mapSafeVec :: (a -> b) -> Vector n a -> Vector n b
+mapSafeVec _ VNil = VNil
+mapSafeVec f (x :> xs) = f x :> mapSafeVec f xs
+
+index :: Fin n -> Vector n a -> a
+index FinZ (x :> _) = x
+index (FinS idx) (_ :>  xs) = index idx xs
+
+--- | Converts ByteString to internal expected model
+splitLinesAdapter :: Char -> BL.ByteString -> [[String]]
+splitLinesAdapter delim s = split delim <$> (lines . elements) s
+
+-- |Get Column by index number
+getColumnByIndex :: Fin c -> CSVFile i c l -> Vector l String
+getColumnByIndex i (NameIndexed _ r) = mapSafeVec (\c -> index i c) r
+getColumnByIndex i (NumberIndexed r) = mapSafeVec (\c -> index i c) r
+
+-- | Get Row
+getRow :: Fin l -> CSVFile i c l -> Vector c String
+getRow i (NameIndexed _ r) = index i r
+getRow i (NumberIndexed r) = index i r
+
+-- | Get Headers
+getHeaders :: CSVFile Name c l -> Vector c String
+getHeaders (NameIndexed h _) = h
+
+--- >>> v = NameIndexed ("Nome" :> "Idade" :> VNil) (("Lucas" :> "24" :> VNil) :> ("Geo" :> "23" :> VNil) :> VNil)
+--- >>> getColumnByIndex (FinS (FinS FinZ)) v
+
+-- strToMatriz :: [[String]] -> Vector n (Vector m String)
+-- strToMatriz = undefined
+
+decode :: FileType indexT -> BL.ByteString -> Maybe (CSVFile indexT lin col)
+decode Header s = case splitLinesAdapter ',' s of
+    (header : xs) -> Just $ NameIndexed header (strToMatriz xs)
+    _ -> Nothing
+decode NoHeader s = Just $ NumberIndexed $ strToMatriz $ splitLinesAdapter ',' s
+
+-- somefunc :: IO ()
+-- somefunc = do
+--     csvData <- BL.readFile "test.csv"
+--     case decode Header csvData of
+--         Nothing -> putStrLn "Error on decode"
+--         Just v -> print v
+
+
+
